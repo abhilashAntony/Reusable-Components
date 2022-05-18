48a95e68dc4bceee8d3a3fb8b0530aee9156b2eb
Abhilash Antony <abhilashant13@gmail.com>
2022-05-18 23:11:11 +0530

    Created main-page and reusable components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+import { CountDownTimerComponent } from './count-down-timer/count-down-timer.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomSheetComponent,
+    CountDownTimerComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
