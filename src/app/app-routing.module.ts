c3d2a8715c22cca54bbb3c9b6ff5d56bb7c562dd
Abhilash Antony <abhilashant13@gmail.com>
2022-05-18 23:55:01 +0530

    Added navigation page. Configured routes for re-usable component pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+import { CountDownTimerComponent } from './count-down-timer/count-down-timer.component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainPageComponent, children: [
+      { path: 'bottom-sheet', component: BottomSheetComponent },
+      { path: 'count-down-timer', component: CountDownTimerComponent },
+      { path: '', component: MainNavigationComponent}
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 import { CountDownTimerComponent } from './count-down-timer/count-down-timer.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BottomSheetComponent,
     CountDownTimerComponent,
-    MainPageComponent
+    MainPageComponent,
+    MainNavigationComponent
   ],
   imports: [
     BrowserModule,
